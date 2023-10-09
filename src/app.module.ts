610c257486783aff197de6a86839f6ddb0e73c9a
Fernando Nicolás <fersanti2896@gmail.com>
2023-10-08 18:44:13 -0600

    Requiriendo el token en toda la aplicación.

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,20 +8,41 @@ import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin
 import { ItemsModule } from './items/items.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [ 
     ConfigModule.forRoot(),
-    GraphQLModule.forRoot<ApolloDriverConfig>({
+    // TODO: Configuración básica de GraphQL
+    // GraphQLModule.forRoot<ApolloDriverConfig>({
+    //   driver: ApolloDriver,
+    //   // debug: false,
+    //   playground: false,
+    //   autoSchemaFile: join( process.cwd(), 'src/schema.gql'),
+    //   plugins: [
+    //     ApolloServerPluginLandingPageLocalDefault()
+    //   ], 
+    //   includeStacktraceInErrorResponses: false // Activar en producción
+    // }), 
+    GraphQLModule.forRootAsync({
       driver: ApolloDriver,
-      // debug: false,
-      playground: false,
-      autoSchemaFile: join( process.cwd(), 'src/schema.gql'),
-      plugins: [
-        ApolloServerPluginLandingPageLocalDefault()
-      ], 
-      includeStacktraceInErrorResponses: false // Activar en producción
-    }), 
+      imports: [ AuthModule ],
+      inject: [ JwtService ],
+      useFactory: async( jwtService: JwtService ) => ({
+        playground: false,
+        autoSchemaFile: join( process.cwd(), 'src/schema.gql'),
+        plugins: [
+          ApolloServerPluginLandingPageLocalDefault()
+        ], 
+        context({ req }) {
+          const token = req.headers.authorization?.replace('Bearer ', '');
+          if( !token ) throw Error('Se necesita token');
+
+          const payload = jwtService.decode( token );
+          if( !payload ) throw Error('Token no válido');         
+        }
+      })
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
